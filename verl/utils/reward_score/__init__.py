63be503f277e62d97022ad930052447a33006fb3
yukangZhuu <zhuyukang2000@gmail.com>
2026-04-02 21:29:57 +0800

    fix: reward scorer register

--- verl/utils/reward_score/__init__.py
+++ verl/utils/reward_score/__init__.py
@@ -56,7 +56,7 @@ def default_compute_score(
 
         # from . import math_verify
         # res = math_verify.compute_score(solution_str, ground_truth)
-    elif data_source in ["math_dapo", "math", "math_dapo_reasoning"] or data_source.startswith("aime"):
+    elif data_source in ["math_dapo", "math", "math_dapo_reasoning", "cgrpo"] or data_source.startswith("aime"):
         from . import math_dapo
 
         res = math_dapo.compute_score(solution_str, ground_truth)
