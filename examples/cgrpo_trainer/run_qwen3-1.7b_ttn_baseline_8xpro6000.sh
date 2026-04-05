378f28d2b3038f0d3702d55c311bf75f917a45a5
yukangZhuu <zhuyukang2000@gmail.com>
2026-04-06 03:57:31 +0800

    fix: oom

--- examples/cgrpo_trainer/run_qwen3-1.7b_ttn_baseline_8xpro6000.sh
+++ examples/cgrpo_trainer/run_qwen3-1.7b_ttn_baseline_8xpro6000.sh
@@ -47,7 +47,7 @@ python3 -m verl.trainer.main_cgrpo \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=128 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
@@ -60,7 +60,7 @@ python3 -m verl.trainer.main_cgrpo \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.65 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.62 \
     actor_rollout_ref.rollout.n=8 \
     actor_rollout_ref.rollout.temperature=0.6 \
     actor_rollout_ref.rollout.top_p=0.95 \
