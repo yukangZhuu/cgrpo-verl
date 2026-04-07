04df050e10b203230a98aa8be59117d13c6fb90a
yukangZhuu <zhuyukang2000@gmail.com>
2026-04-08 02:30:32 +0800

    feat: update

--- examples/cgrpo_trainer/run_ttn2k_baseline_8xpro6000.sh
+++ examples/cgrpo_trainer/run_ttn2k_baseline_8xpro6000.sh
@@ -50,7 +50,7 @@ python3 -m verl.trainer.main_cgrpo \
     actor_rollout_ref.actor.ppo_mini_batch_size=128 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
-    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=36000 \
+    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=48000 \
     actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
